dad6636198a9547d06e10c69920dc67d7b63d48f
Aman Arora <aroraaman1803@gmail.com>
2024-01-09 15:51:29 +0530

    initial commit

---
+++ src/assets/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+    return (
+        <div>
+            <nav>
+                <div className="logo">
+                    <img src="/images/brand_logo.png" alt="" />
+                </div>
+            </nav>
+
+        </div>
+    );
+};
+
+export default App;
